a7f897d18bb04d53bc2b538fc37f373886e425ff
Leandro Aranda <learanda@hotmail.com.ar>
2022-07-12 15:21:13 -0300

    Added viewBinding and dependencies

--- andMarvelKMM/build.gradle.kts
+++ andMarvelKMM/build.gradle.kts
@@ -1,13 +1,16 @@
+import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
+
 plugins {
     id("com.android.application")
     kotlin("android")
+    kotlin("kapt")
 }
 
 android {
     compileSdk = 32
     defaultConfig {
         applicationId = "ar.leandro.talleravanzadolean.android"
-        minSdk = 21
+        minSdk = 24
         targetSdk = 32
         versionCode = 1
         versionName = "1.0"
@@ -17,11 +20,28 @@ android {
             isMinifyEnabled = false
         }
     }
+    buildFeatures {
+        viewBinding = true
+    }
     buildToolsVersion = "32.0.0"
 }
 
 dependencies {
+    implementation(project(":shared"))
+
     implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+    implementation("androidx.core:core-ktx:1.6.0")
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.0")
+    // RecyclerView
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0-alpha03")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0-alpha03")
+    kapt("androidx.lifecycle:lifecycle-compiler:2.4.0-alpha03")
+    // Picasso
+    implementation("com.squareup.picasso:picasso:2.71828")
 }
